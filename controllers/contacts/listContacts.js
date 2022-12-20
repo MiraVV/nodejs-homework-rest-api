9a367cbd6964dafd8b86067bd73a0be76d3bbaf2
Myroslava <v16m06a@gmail.com>
2022-12-20 22:02:45 +0100

    favorite: true

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -4,7 +4,7 @@ const listContacts = async (req, res, next) => {
   const { _id } = req.user;
   const { page = 1, limit = 20 } = req.query;
   const skip = (page - 1) * limit;
-  const result = await Contact.find({ owner: _id }, "", {
+  const result = await Contact.find({ owner: _id, favorite: true }, "", {
     skip,
     limit: Number(limit),
   }).populate("owner", "_id email");
